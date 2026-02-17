d8150498a4b89c521678504f34ee69f616649fbc
1ajthomas248 <1ajthomas248@gmail.com>
2026-02-17 11:52:22 -0500

    added NSG with Allow SSH rule and associate with subnet

--- vnet-subnet/main.tf
+++ vnet-subnet/main.tf
@@ -35,3 +35,30 @@ resource "azurerm_subnet" "subnet" {
 
   address_prefixes = [var.subnet_cidr]
 }
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-tf-lab02"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  tags = var.tags
+}
+
+resource "azurerm_network_security_rule" "allow_ssh" {
+  name                        = "Allow-SSH"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
